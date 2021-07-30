8df257d57feba97f0f88a981f633323060e112a6
Sandor Simon <sandor_simon@epam.com>
2021-07-30 17:11:25 +0200

    fix a few assertation mistake

--- mocha/specs/career.spec.js
+++ mocha/specs/career.spec.js
@@ -10,7 +10,7 @@ const write = require('fs').writeFile
 describe('Career page',function(){
     this.timeout(55000);
     before(function(){
-        this.page = new CareerPage()
+        this.page = new CareerPage();
         })
     //after(function(){browser.close()})
    
@@ -26,53 +26,50 @@ describe('Career page',function(){
     //     }
         
     // })
-
-    // itt az első verzió ahol kis lépésenként jönnek a tesztek
     it('should load',async function () {
-        await browser.get(careerPageUrl)
-        expect(await this.page.isLoaded()).to.be.true
+        await browser.get(careerPageUrl);
+        expect(await this.page.isLoaded()).to.be.true;
      });
-    
 
     it('search form should be reachable', async function () {
-        const page = this.page
-        expect(await page.isJobApplyFormPresent()).to.be.true
-        
+        const page = this.page;
+        expect(await page.isJobApplyFormPresent()).to.be.true;
      });
 
      it('The location filter box should contain City', async function () {
-        const page = this.page
-        await page.click(page.locators.css.locationSearch)
+        const page = this.page;
+        await page.click(page.locators.css.locationSearch);
         browser.wait(EC.presenceOf(element(by.css(page.getCitySelector("Debrecen")))), 5000);
-        const city = element(by.css(page.getCitySelector("Debrecen")))
-        expect(city).not.to.be.undefined;
-        browser.executeScript(`document.querySelector("${page.getCitySelector("Debrecen")}").setAttribute("selected","true")`)
+        const isCityPresent = await element(by.css(page.getCitySelector("Debrecen"))).isPresent();
+        expect(isCityPresent).to.be.true;
+        browser.executeScript(`document.querySelector("${page.getCitySelector("Debrecen")}").setAttribute("selected","true")`);
      });
 
      it('Should have a way to select a skill', async function () {
-        const page = this.page
-        const skills = await element(by.css(page.locators.css.skills))
-        expect(skills).not.to.be.undefined;
-        browser.executeScript(`document.querySelector("${page.getSkillLocator('Software Test Engineering')}").click()`)
-        const findBtn = await element(by.css(this.page.locators.css.findBtn))
-        findBtn.click()
+        const page = this.page;
+        const isSkillSelectPresent = await element(by.css(page.locators.css.skills)).isPresent();
+      
+        expect(isSkillSelectPresent).to.be.true;
+        browser.executeScript(`document.querySelector("${page.getSkillLocator('Software Test Engineering')}").click()`);
+        const findBtn = await element(by.css(this.page.locators.css.findBtn));
+        findBtn.click();
       
      });
 
      it('Search result should contain the given position', async function () {
-        const page = this.page
-        const resultList = await page.getResultList()
-        this.matchedPositions = await page.getMatchedResults(resultList,'Lead Test Automation Engineer')
-        expect(this.matchedPositions).has.length.greaterThan(0)
+        const page = this.page;
+        const resultList = await page.getResultList();
+        this.matchedPositions = await page.getMatchedResults(resultList,'Lead Test Automation Engineer');
+        expect(this.matchedPositions).has.length.greaterThan(0);
      });
 
      it('Every position should have an apply button', async function () {
-        const page = this.page
+        const page = this.page;
         for (let element of this.matchedPositions){
-            const location = await page.getLocation(element)
-            const applyButton = await element.element(by.css(page.locators.css.applyBtn))
-            expect(location.toUpperCase()).to.include('Debrecen'.toUpperCase())
-            expect(applyButton).not.to.be.undefined
+            const location = await page.getLocation(element);
+            const applyButton = await element.element(by.css(page.locators.css.applyBtn));
+            expect(location.toUpperCase()).to.include('Debrecen'.toUpperCase());
+            expect(applyButton).not.to.be.undefined;
         }
      });
 });
